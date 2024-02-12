fa56bed10e9f64e085152b77804c414e2287a433
Joshua Balao <JoshuaCBalao@gmail.com>
2024-02-12 15:50:20 -0500

    Routes
    added Routes for pathing NAVBAR

--- src/App.js
+++ src/App.js
@@ -2,16 +2,34 @@
 import './App.css';
 import './index.css';
 import Navbar from './Components/Assests/Navbar/navbar.jsx';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Shop from './Pages/Shop.jsx'
+import ShopCategory from './Pages/ShopCategory.jsx'
+import Product from './Pages/Product.jsx'
+import Cart from './Pages/Cart.jsx'
+import Login from './Pages/LoginSignup.jsx'
 
 
 
 function App() {
     return (
-    <div className='App'>
-      
-      <Navbar/>
-     
-     
+    <div>
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+      <Route path='/' element={<Shop/>}/>
+      <Route path='/tumbler' element={<ShopCategory/>}/>
+      <Route path='/Acrylics' element={<ShopCategory/>}/>
+      <Route path='/Metals' element={<ShopCategory/>}/>
+      <Route path="/Product" element={<Product/>}>
+        <Route path=':productId' element={<Product/>}/>
+      </Route>
+      <Route path='/Cart' element={<Cart/>}/>
+      <Route path= '/Login' element={<LoginSignup/>}/>
+
+    </Routes>
+    </BrowserRouter>
+    
       
     </div>
   );
--- src/Components/Assests/Hero/Hero.jsx
+++ src/Components/Assests/Hero/Hero.jsx
@@ -1,3 +1,8 @@
+import React from 'react'
+import './Hero.css'
+import hand_icon from '../Images/hand_icon.png'
+import arrow_icon from '../Images/Arrow Icon.png'
+import hero_image from '../Images/test Hero Image.jpg'
 
 
 const Hero = () => {
@@ -6,18 +11,21 @@ const Hero = () => {
             <div className= 'hero-left'>
                 <h2>QUALITY AND PRECISION</h2>
                     <div>
-                        <div className='hero-hand-icon'>
+                        <div className='hand-hand-icon'>
                             <p>The Best Materials</p>
-                            <img src="hand-icon" alt="" />
+                            <img src={hand_icon.png} alt="" />
                         </div>
                         <p>chosen to</p>
-                        <p>make you</p>
+                        <p>make your</p>
                         <p>memories timless</p>
                     </div>
                     <div className="hero-latest-btn"></div>
                     <div>Our Materials</div>
                     <img src={arrow_icon} alt="" />
-            </div>           
+            </div> 
+            <div className= "hero-right">
+                <img src={hero_image} alt="" />
+            </div>         
         </div>
     )
 }
