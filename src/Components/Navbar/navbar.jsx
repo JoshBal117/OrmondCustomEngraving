6c5ef0d78e209cfdda6b11d47bd56a87e759d9bf
Joshua Balao <JoshuaCBalao@gmail.com>
2024-02-16 22:27:30 -0500

    Updated the Menu
    updated highlight for menu and set click for login button; set also numerical counter for items in cart

--- src/Components/Navbar/navbar.jsx
+++ src/Components/Navbar/navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react"; 
 import './Navbar.css';
 import Logo from '../Assests/Images/Logo.png';
 import cart_icon from '../Assests/Images/cart-icon.png';
@@ -6,21 +6,25 @@ import cart_icon from '../Assests/Images/cart-icon.png';
 
 
 const Navbar = () => {
+
+    const [menu,setMenu] = useState("shop");
+
   return (
     <nav className="navbar">
       <div className="nav-logo">
         <img src={Logo} alt="" />
       </div>
       <ul className="nav-menus">
-        <li>Shop<hr/></li>
-        <li>Awards</li>
-        <li>Tumblers</li>
-        <li>Stones</li>
+        <li onClick={()=>{setMenu("shop")}}>Shop{menu==="shop"?<h/>:<></>}</li>
+        <li onClick={()=>{setMenu("awards")}}>Awards{menu==="awards"?<h/>:<></>}</li>
+        <li onClick={()=>{setMenu("tumblers")}}>Tumblers{menu==="tumblers"?<h/>:<></>}</li>
+        <li onClick={()=>{setMenu("stones")}}>Stones{menu==="stones"?<h/>:<></>}</li>
         
       </ul>
       <div className="nav-login-cart">
         <button>Login</button>
         <img src={cart_icon} alt=""/>
+        <div className="nav-cart-count">0</div>
       </div>
     </nav>
   )
