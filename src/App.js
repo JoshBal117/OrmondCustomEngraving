3266c2714db24976ede5cb37ca02e6239a52f1eb
Joshua Balao <JoshuaCBalao@gmail.com>
2024-02-17 09:18:43 -0500

    loginsignup
    corrected loginsignup path

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@
 import './App.css';
 import Navbar from './Components/Navbar/navbar.jsx'
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
-import Shop from './Pages/Shop'
+import Shop from './Pages/Shop';
 import ShopCategory from './Pages/ShopCategory';
+import Product from './Pages/Product.jsx';
+import Cart from './Pages/Cart.jsx;'
+import LoginSignup from './Pages/LoginSingup.jsx'
     
 
 function App() {
@@ -16,7 +19,11 @@ function App() {
           <Route path='/awards' element={<ShopCategory/>}/>
           <Route path='/tumblers' element={<ShopCategory/>}/>
           <Route path='/stones' element={<ShopCategory/>}/>
-         
+         <Route path= '/product' element={<Product/>}>
+            <Route path=':productId' element={<Product/>}/>
+         </Route>
+         <Route path='/cart' element={<Cart/>}/>
+         <Route path= '/login' element={<LoginSignup/>}/>
       </Routes>
       </BrowserRouter>
    
