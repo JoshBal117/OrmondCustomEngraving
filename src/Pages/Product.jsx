004e895682d0ff9369fd7d9a07b9666cf8c95ce3
Joshua Balao <JoshuaCBalao@gmail.com>
2024-02-26 17:41:14 -0500

    added new component
    added new folder and files for product descriptions; added and edited new login sign up pages as well,

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -3,6 +3,7 @@ import { ShopContext } from "../Context/Shopcontext";
 import { useParams } from "react-router-dom";
 import Breadcrumb from "../Components/Breadcrumb/Breadcrumb";
 import ProductDisplay from "../Components/ProductDisplay/ProductDisplay";
+import DescriptionBox from "../Components/DescriptionBox/descriptionbox";
 
 const Product = () => {
     const {all_product}= useContext(ShopContext);
@@ -12,6 +13,7 @@ const Product = () => {
         <div>
             <Breadcrumb product={product}/>
             <ProductDisplay product={product}/>
+            <DescriptionBox/>
         </div>
     )
 }
