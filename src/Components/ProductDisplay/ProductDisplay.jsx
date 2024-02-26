5d8e8472ade4f36a3466ed15c483143b57a6242e
Joshua Balao <JoshuaCBalao@gmail.com>
2024-02-25 22:45:56 -0500

    Update ProductDisplay.jsx
    Updated rhte product display file after creation of said file for app.

--- src/Components/ProductDisplay/ProductDisplay.jsx
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 
-const ProductDisplay = () => {
+const ProductDisplay = (props) => {
+    const {product} = props;
   return (
     <div className='productdisplay'>
-        
+        <div className="prodisplay-left">
+            <div className="productdisplay-img-list">
+                <img src={product.img} alt="" />
+                <img src={product.img} alt="" />
+                <img src={product.img} alt="" />
+                <img src={product.img} alt="" />
+            </div>
+            <div className="productdisplay-img">
+                <img className='productdisplay-main-img'src={product.image} alt="" />
+            </div>
+        </div>
+        <div className="displayproduct-right">
+            <h1>{product.name}</h1>
+            
+           
+        </div>
     </div>
   )
 }
