32dd90f0ca30deff95d07fefb9410ff2b31682cc
Joshua Balao <JoshuaCBalao@gmail.com>
2024-02-25 21:41:51 -0500

    login Signup CSS
    created the full file for CSS of thesign up page to ensure brilliant looking user interface

--- src/Pages/LoginSingup.jsx
+++ src/Pages/LoginSingup.jsx
@@ -7,7 +7,7 @@ const LoginSingup = () => {
             <div className="loginsignup">
                 <div className="loginsignup-container">
                     <h1>Sign Up</h1>
-                    <div className="loginsignup-field">
+                    <div className="loginsignup-fields">
                         <input type="text" placeholder='Your Name' />
                         <input type="text" placeholder='Email Address' />
                         <input type="text" placeholder='Password' />
