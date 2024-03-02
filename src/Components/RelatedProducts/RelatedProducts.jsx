0ca7a10f6331815e14eccbdca76ea661649dcdaa
Joshua Balao <JoshuaCBalao@gmail.com>
2024-03-01 20:52:33 -0500

    css and component
    aded the CSS and code lines for related products componnent; will check CSS after the  placement of the component within the app

--- src/Components/RelatedProducts/RelatedProducts.jsx
+++ src/Components/RelatedProducts/RelatedProducts.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import './RelatedProducts.css'
-
+import data_product from '../Assests/Data/data'
+import Item from '../item/item'
 const RelatedProducts = () => {
   return (
-    <div></div>
+    <div>
+        <h1>Related Products</h1>
+        <hr />
+        <div className="relatedproducts-item">
+            {data_product.map((item,i) =>{
+                return <Item key={i} id={item.id}  name={item.name} image={item.image} current_price={item.current_price}/>
+            })}
+        </div>
+    </div>
   )
 }
 
